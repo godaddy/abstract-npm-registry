72a04f00d83fb86c424fdf70758dc49c9464e262
indexzero <charlie.robbins@gmail.com>
2016-03-08 01:51:33 -0800

    [api] Added tests for updating package metadata (e.g. README).

---
+++ pkg/update.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Test coverage for updating JSON of npm packages
+ * See: npm-registry-couchapp
+ * https://github.com/npm/npm-registry-couchapp/blob/master/registry/rewrites.js#L89-L90
+ */
+module.exports = function (opts) {
+  return function () {
+    throw new Error('Not implemented.');
+  };
+};
