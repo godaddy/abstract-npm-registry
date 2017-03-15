f5aac02cb2a5dc62f7ca2f9a6cb2b6b196a95b8c
indexzero <charlie.robbins@gmail.com>
2017-03-14 17:02:04 -0700

    [api] Tests for (now deprecated) `/-/all`

--- views/all.js
+++ views/all.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const assume = require('assume');
 const nit = require('../lib/nit')('GET /-/all');
+const requests = require('../lib/requests');
 
 /**
  * Test coverage for listing all packages
@@ -8,14 +10,51 @@ const nit = require('../lib/nit')('GET /-/all');
  * https://github.com/npm/npm-registry-couchapp/blob/master/registry/rewrites.js#L24
  * https://github.com/npm/npm-registry-couchapp/blob/master/registry/rewrites.js#L32-L33
  */
-module.exports.full = nit.skip(':api', function (opts) {
-  return function () {
-    throw new Error('Not implemented.');
+module.exports.index = nit(':api', function (opts) {
+  return function (done) {
+    requests.json({
+      host: opts.registry,
+      path: '/-/all/',
+      method: 'GET',
+      status: 404
+    }, done);
   };
 });
 
-module.exports.since = nit.skip(':api/since', function (opts) {
-  return function () {
-    throw new Error('Not implemented.');
+module.exports.since = nit(':api/since', function (opts) {
+  return function (done) {
+    requests.go({
+      host: opts.registry,
+      path: '/-/all/since',
+      method: 'GET',
+      status: 302,
+      expect: function (res) {
+        assume(res.headers.location).equals(
+          `${opts.registry}/-/all/static/all.json`
+        );
+      }
+    }, done);
+  };
+});
+
+module.exports.static = nit(':api/static/all.json', function (opts) {
+  return function (done) {
+    var superquest = requests.json({
+      host: opts.registry,
+      path: '/-/all/static/all.json',
+      method: 'GET',
+      status: 200,
+      expect: function (res) {
+        assume(res.headers['content-length']).is.a('number');
+      }
+    }, done);
+
+    //
+    // This returns more than 100MB of JSON so we want to end it ASAP
+    // since we are only asserting headers and response code.
+    //
+    superquest.req.on('response', function (res) {
+      res.end();
+    });
   };
 });
